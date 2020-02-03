310a79cdf83af22567ec2457936258618b95b89a
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-03 17:04:24 -0300

    remoção de componentes não utilizados

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
+
 import '../App.css'
 
 const NavBar = () => {
