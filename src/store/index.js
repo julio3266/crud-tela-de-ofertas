f142281c77ae57d77b8f32d177d74a88a168b35b
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-06 22:54:52 -0300

    remoção redux

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import { modeloReducer } from '../store/modeloReducer'
-const mainReducer = combineReducers({
-    tabela: modeloReducer
-})
-
-export default mainReducer
