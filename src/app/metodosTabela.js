f337ca03f7418ddec8a7d3a38b1393559972bcc9
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-06 22:54:21 -0300

    inserção metodosTabela

---
+++ src/app/metodosTabela.js
@@ -0,0 +1,23 @@
+const PRODUTOS = '_PRODUTOS'
+export default class metodosTabela {
+
+
+        getProdutos = () => {
+            const produtos = localStorage.getItem(PRODUTOS)
+            return JSON.parse(produtos)
+        }
+
+    salvar = (produto) => {
+        let produtos = localStorage.getItem(PRODUTOS)
+        
+        if(!produtos) {
+            produtos = []
+        } else{
+            produtos = JSON.parse(produtos)
+        }
+
+        produtos.push(produto)
+        
+        localStorage.setItem(PRODUTOS, JSON.stringify(produtos))
+    }
+}
