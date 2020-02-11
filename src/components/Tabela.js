e754b80f7009c42971e3452a711a1da0bdfddda3
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-11 16:39:46 -0300

    buscar e setBuscar e on Change dentro do search

--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -8,17 +8,23 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import firebase from '../Config/firebase'
+import TextField from '@material-ui/core/TextField'
 
 function Tabela(key, marca, modelo, data) {
+    const [buscar, setBuscar] = useState('');
     const [ofertas, setOfertas] = useState([]);
     const listaofertas = []
+    
     useEffect(()=>{
         firebase.firestore().collection('ofertas').get().then(async (resultado) => {
             await resultado.docs.forEach(doc => {
+                if(doc.data().modelo.indesOf(buscar) >= 0)
+                {
                 listaofertas.push({
                     id: doc.id,
                     ...doc.data()
                 })
+              }
             })
             setOfertas(listaofertas);
         })
@@ -27,6 +33,11 @@ function Tabela(key, marca, modelo, data) {
     return(
     <Paper elevation={2} style={{ marginTop: 20 }}>
         <Grid contianer>
+            <Grid md item={8}>
+                <form style={{marginTop: 30}} noValidate autoComplete="off">
+                    <TextField onChange={(e) => setBuscar(e.target.value)} style={{ marginTop: 20, marginLeft: 3 }}   label="Buscar" variant="outlined" />
+                </form>
+            </Grid>
             <Grid item={12}>
                 <Table>
                     <TableHead>
