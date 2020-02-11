fe13e8ca16a853bb2cd590027c6f85c7c971f871
Julio Valente Costa <juliovalente321@gmail.com>
2020-02-11 19:21:49 -0300

    apenas uma linha

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import BuscarEAdd from '../components/BuscarEAdd'
 import Tabela from '../components/Tabela'
+
 class Home extends Component {
     render(){
         return(
