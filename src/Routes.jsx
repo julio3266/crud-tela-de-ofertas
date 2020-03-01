ab195082409d25866c24d7bb0b5c674e86be809b
Julio Valente Costa <juliovalente321@gmail.com>
2020-03-01 11:02:32 -0300

    layout login, routes login e component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,13 @@ import { Route, Switch} from 'react-router-dom'
 import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
 import Cadastro from './Pages/Cadastro'
-
+import Login from './Pages/Login'
 class Routes extends Component {
     render(){
         return (         
                 <Switch>
                     <Route exact path='/' component={Home} />
+                    <Route exact path='/Login' component={Login} />
                     <Route exact path='/Cadastro' component={Cadastro} />
                     <Route exact path='*' component={NotFound} />
                 </Switch>     
---
+++ src/components/FormLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import{ Grid, TextField, Button} from '@material-ui/core';
+import {Link} from 'react-router-dom'
+import CustomFormLogin from '../Custon/CustomFormLogin.css'
+
+export default function FormLogin(props){
+    return(
+        <Grid 
+         container
+         spacing={2}
+         direction="column"
+         alignItems="center"
+         justify="center"
+         style={{ minHeight: '90vh'  }}
+        >
+            <Grid item xs={7}>
+                    <TextField 
+                    className="inputLogin"
+                    style={{marginBottom: 10}}
+                    fullWidth
+                    variant="outlined"
+                    label="Usuário"
+                    color="secondary"
+                    />
+                    <TextField
+                    style={{marginBottom: 10}}
+                        fullWidth  
+                    variant="outlined"
+                    type="password"
+                    label="Senha"
+                    color="secondary"
+                    />
+            </Grid>      
+            <Grid  item md={3}>
+                <Button variant="contained" style={{ marginRight: 5 }} color="primary" size="medium">Entrar</Button>
+                <Button variant="contained" color="secondary" size="medium">Cadastrar</Button>
+            </Grid>
+         
+        </Grid>
+    );
+  }
+
+    
+ 
