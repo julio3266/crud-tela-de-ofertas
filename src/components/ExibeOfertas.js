889d3593edc571b9c4fce6d20df0caec8a72e638
Julio Valente Costa <juliovalente321@gmail.com>
2020-03-02 12:06:05 -0300

    Ofertas tela front

--- src/components/ExibeOfertas.js
+++ src/components/ExibeOfertas.js
@@ -1,7 +1,176 @@
 import React from 'react'
-import {Typography} from '@material-ui/core'
+import {Paper, Grid, Typography, Card, CardHeader, CardContent, CardActionArea, CardMedia} from '@material-ui/core'
+
 export default function ExibeOferta(){
     return(
-        <Typography variant="h6">Teste</Typography>
+        <Grid 
+        spacing={1}
+        style={{ marginTop: 80 }}
+        container 
+        direction="row"
+        justify="space-evenly"
+        alignItems="baseline"
+        >
+            <Grid item sm={3}>
+               <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 1
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                    <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://via.placeholder.com/100x50"
+                                title="Onix Joy"        
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Modelo 2
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                    </CardActionArea>
+                 </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+            <Grid item sm={3}>
+                <Card>
+                   <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            image="https://via.placeholder.com/100x50"
+                            title="Onix Joy"        
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Modelo 3
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                   </CardActionArea>
+               </Card>
+            </Grid>
+        </Grid>
     )
 }
